fc1eff65fe4f6210bd7c2388e4a840ee45fa9664
fakhri <irhkafodir@gmail.com>
2025-06-04 16:22:50 +0700

    fix(eslint): update config for TypeScript support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 } 
